7dd6734b179a42bf08c247988f1cf89d9229979c
Soubhagya264 <71813414+Soubhagya264@users.noreply.github.com>
2025-05-22 20:16:27 +0530

    Update DB Configure

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,21 +1,23 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient | undefined
+  var prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient({
-  log: ['error', 'warn'],
-  errorFormat: 'pretty',
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL + '?connection_limit=5&pool_timeout=30'
-    }
-  }
-})
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["error", "warn"],
+    errorFormat: "pretty",
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL, // Clean and portable
+      },
+    },
+  });
 
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
 }
 
-export default prisma 
+export default prisma;
